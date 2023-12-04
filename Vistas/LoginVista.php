a024fe9a8a8add5e9b3ecabd4cbbc8af9ceb3b52
robmab <rob_mb@outlook.es>
2023-12-04 11:13:14 +0100

    Login | Refactor CSS

--- Vistas/LoginVista.php
+++ Vistas/LoginVista.php
@@ -3,6 +3,7 @@
 
 <head>
   <?php include "../Tema/CSS.php" ?>
+  <link rel="stylesheet" href="../css/views/login.css">
   <title>AutoCon - Login</title>
 </head>
 
@@ -10,66 +11,63 @@
   <?php include "../Tema/Menu.php" ?>
 
   <!-- HERO -->
-  <div class="hero-area bg-img background-overlay" 
-      style="background-image:url(../img/blog-img/login.jpg);
-      height:150px" ?>></div>
+  <div class="hero-area bg-img background-overlay" style="background-image:url(../img/blog-img/login.jpg);
+      height:150px" ?></div>
 
   <!-- BODY -->
-  <section class="contact-area section-padding-100" style="padding:0;margin:2em 0;"">
+  <section class="login contact-area section-padding-100">
     <div class=" container">
-    <div class=" row justify-content-center">
+      <div class=" row justify-content-center">
 
-      <!-- Contact Form Area -->
-      <div class="col-12 col-md-10 col-lg-8">
-        <div class="contact-form" style="padding-top:1.5em;display:flex;
-      justify-content:center;flex-direction:column;">
-          <h1 style=" display: flex; justify-content: center; margin-bottom: 1em;  ">Iniciar Sesión</h1>
+        <!-- Contact Form Area -->
+        <div class="col-12 col-md-10 col-lg-8">
+          <div class="contact-form">
+            <h1>Iniciar Sesión</h1>
+            <!-- Contact Form -->
+            <form action="../Controladores/LoginControlador.php<?php if (isset($_SESSION['mensajeBD'])) {
+              if ($_SESSION['mensajeBD'] == 'Tienes que registrarte antes de poder comprar o alquilar un vehículo.') {
+                echo "?compraV=1";
+              }
+            } ?>" method="post">
 
-          <!-- Contact Form -->
-          <form action="../Controladores/LoginControlador.php<?php if (isset($_SESSION['mensajeBD'])) {
-            if ($_SESSION['mensajeBD'] == 'Tienes que registrarte antes de poder comprar o alquilar un vehículo.') {
-              echo "?compraV=1";
-            }} ?>" method="post">
-          
-            <div class="row" style="display: flex; justify-content: center; 
-            align-items:center; flex-direction: column; width: 100%; margin: 0;">
+              <div class="row">
 
-              <!-- ERROR MESSAGE -->
-              <?php if (isset($_SESSION['mensajeBD'])) {
-                echo "<p style='color:red;font-weight:800;'> " . $_SESSION['mensajeBD'] . "</p> ";
-                unset($_SESSION['mensajeBD']);
-              } ?>
+                <!-- ERROR MESSAGE -->
+                <?php if (isset($_SESSION['mensajeBD'])) {
+                  echo "<p class='alert'> " . $_SESSION['mensajeBD'] . "</p> ";
+                  unset($_SESSION['mensajeBD']);
+                } ?>
 
-              <div class="col-12 col-md-6">
-                <div class="group">
-                  <input type="text" name="email" id="email" required autocomplete="new-password" value="">
-                  <span class="highlight"></span>
-                  <span class="bar"></span>
-                  <label>Nombre de usuario o dirección</label>
+                <div class="col-12 col-md-6">
+                  <div class="group">
+                    <input type="text" name="email" id="email" required autocomplete="new-password" value="">
+                    <span class="highlight"></span>
+                    <span class="bar"></span>
+                    <label>Nombre de usuario o dirección</label>
+                  </div>
                 </div>
-              </div>
-              <div class="col-12 col-md-6">
-                <div class="group">
-                  <input type="password" name="pass" id="pass" required autocomplete="new-password" value="">
-                  <span class="highlight"></span>
-                  <span class="bar"></span>
-                  <label>Contraseña</label>
+                <div class="col-12 col-md-6">
+                  <div class="group">
+                    <input type="password" name="pass" id="pass" required autocomplete="new-password" value="">
+                    <span class="highlight"></span>
+                    <span class="bar"></span>
+                    <label>Contraseña</label>
+                  </div>
+
                 </div>
-
+                <a href="RegistroVista.php">
+                  <p>¡Regístrate si aún no lo estás!</p>
+                </a>
               </div>
-              <a href="RegistroVista.php">
-                <p style='color:royalblue; font-size: 12px;' ;>¡Regístrate si aún no lo estás!</p>
-              </a>
-            </div>
 
-            <!-- BUTTON -->
-            <div class="col-12" style="display: flex; justify-content: center;">
-              <button type="submit" class="btn world-btn" style="margin:1em 0 0 0">Entrar</button>
-            </div>
-          </form>
+              <!-- BUTTON -->
+              <div class="but col-12">
+                <button type="submit" class="btn world-btn">Entrar</button>
+              </div> 
+            </form>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   </section>
 
